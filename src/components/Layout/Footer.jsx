debe607c098f89c5f52802d0e21cb15676d96b1d
Georgie Recabo <58155695+grecabo123@users.noreply.github.com>
2023-03-22 11:55:26 +0800

    Added Footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { FaArrowRight, FaEnvelope, FaPen, FaPhone } from 'react-icons/fa'
+import { Divider } from 'primereact/divider'
+import { ProgressBar } from 'primereact/progressbar';
+
+import { FaArrowRight, FaEnvelope, FaMailBulk, FaPen, FaPhone } from 'react-icons/fa'
 
 function Footer() {
     return (
@@ -9,8 +12,31 @@ function Footer() {
                     <div className="col-md-3 mb-2 title">
                         <div className="footer-column">
                             <h4>service advantages</h4>
-                            <h4>deposit</h4>
-                            <h4>withdrawwal</h4>
+                            <div className='service'>
+                                <div className='depo'>
+                                <h4>deposit</h4>
+                                </div>
+                                <div className="d-flex justify-content-between top-move">
+                                    <span className='text-secondary'>average time</span>
+                                    <span className='num'>3 <span className='text-mins'>mins</span></span>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar w-25" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div className='service'>
+                                <div className="depo">
+                                <h4>withdrawal</h4>
+                                </div>
+                                <div className="d-flex justify-content-between top-move">
+                                    <span className='text-secondary'>average time</span>
+                                    <span className='num'>20 <span className='text-mins'>mins</span></span>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar w-50" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                           
                         </div>
                     </div>
                     <div className="col-md-3 mb-2 title">
@@ -23,18 +49,6 @@ function Footer() {
                         </div>
                     </div>
                     <div className="col-md-3 mb-2 title">
-                        {/* <div className="footer-column list-column justify-content-center">
-                            <h4>info center</h4>
-                            <li>
-                                <span className='info'>faq</span> <FaArrowRight size={13} className="text-white" />
-                            </li>
-                            <li>
-                                <span className='info'>how to deposite?</span> <FaArrowRight size={13} className="text-white" />
-                            </li>
-                            <li>
-                                <span className='info'>how to withdraw?</span> <FaArrowRight size={13} className="text-white" />
-                            </li>
-                        </div> */}
                         <div className="row justify-content-center footer-column">
                             <h4>info center</h4>
 
@@ -55,14 +69,131 @@ function Footer() {
                         <div className="row justify-content-center footer-column">
                             <h4>contact us</h4>
                             <div className="footer-contact">
-                                <li><FaPen size={30} className="text-yellow" /> </li>
-                               <li><FaEnvelope  size={30} className="text-yellow"/></li>
-                               <li><FaPhone size={30} className="text-yellow" /></li>
+                                <li><FaPen size={30} className='text-yellow' />
+                                    <div className="list-info">
+                                        <li><span>live chat</span></li>
+                                        <li><a href='/'>click here</a></li>
+                                    </div>
+                                </li>
+                                <li><FaMailBulk size={30} className='text-yellow' />
+                                    <div className="list-info">
+                                        <li><span>mail</span></li>
+                                        <li><small>mail@onlinecasion.com</small></li>
+                                    </div>
+                                </li>
+                                <li><FaPhone size={30} className='text-yellow' />
+                                    <div className="list-info">
+                                        <li><span>hotline</span></li>
+                                        <li><small>(+12) 123-456-7890</small></li>
+                                    </div>
+                                </li>
+
+
+                                {/* <li><FaEnvelope size={20} className="text-yellow" /></li>
+                                <li><FaPhone size={20} className="text-yellow" /></li> */}
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="bg-secondary py-2 mt-4"></div>
+                {/* <div className="border-btm"> */}
+                <div class="d-flex justify-content-between mt-2">
+                    <div>
+                        <div className="footer-list">
+                            <li className='me-3'><span>about us</span></li>
+                            <li className='me-3'><span>responsible gaming</span></li>
+                            <li className='me-3'><span>info centre</span></li>
+                            <li className='me-3'><span>contact us</span></li>
+                            <li className='me-3'><span>affiliates</span></li>
+                        </div>
+                    </div>
+                    <div className='copyright'>
+                        <span>© 2018 Casino All rights reseved | 18+</span>
+                    </div>
+                </div>
+                {/* </div> */}
+                <div className="border-btm"></div>
+
+                <div className="mt-5">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <h5 className='footer-info'>information</h5>
+                            <div className="box-border-btm"></div>
+                            <div className="information-list">
+                                <div>
+                                    <li><h5>registration</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>affiliates</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>responsible gaming</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <h5 className='footer-info'>products</h5>
+                            <div className="box-border-btm"></div>
+                            <div className="information-list">
+                                <div>
+                                    <li><h5>sports betting & live betting</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>online casino</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>live casino</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <h5 className='footer-info'>info center</h5>
+                            <div className="box-border-btm"></div>
+                            <div className="information-list">
+                                <div>
+                                    <li><h5>promotion</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>help centre</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>payment methods</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <h5 className='footer-info'>betting info</h5>
+                            <div className="box-border-btm"></div>
+                            <div className="information-list">
+                                <div>
+                                    <li><h5>sports results</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>betting statistics</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>sport betting</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                                <div>
+                                    <li><h5>casino betting</h5></li>
+                                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste voluptatum labore earum dicta voluptatem omnis quam eaque itaque enim aperiam, tempora id accusamus in iusto quia eum sunt unde.</p></li>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
     )
